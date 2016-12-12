e13fae3790372778437b1259b7415dae3e64ae0c
jramaswami <jramaswami@gmail.com>
2016-12-12 18:46:03 -0500

    Solution to Day 10 Part A using DSL in Racket.

--- 10/dsl/racket/balance-bots/a.rkt
+++ 10/dsl/racket/balance-bots/a.rkt
@@ -12,19 +12,19 @@
 ;; TOKENIZER
 (require parser-tools/lex)
 (define (parse port)
-  (define (helper0 acc)
+  (define (helper0 bot-acc com-acc)
     (let ([token (next-token port)])
-      (if (eof-object? token)
-          (reverse acc)
-          (helper0 (cons token acc)))))
-  (helper0 '()))
+      (cond [(eof-object? token) (append (reverse bot-acc) (reverse com-acc))]
+            [(equal? (car token) 'give) (helper0 bot-acc (cons token com-acc))]
+            [else (helper0 (cons token bot-acc) com-acc)])))
+  (helper0 '() '()))
 
 (define (next-token port)
   (define lxr
     (lexer
      [(eof) eof]
      ["value" `(give ,(number-tokenizer port) ,(bot-tokenizer port))]
-     ["bot" `(bot-instruction ,(number-tokenizer port) ,(bot-tokenizer port) ,(bot-tokenizer port))]
+     ["bot" `(bot-rule ,(number-tokenizer port) ,(bot-tokenizer port) ,(bot-tokenizer port))]
      ["compare" `(compare ,(number-tokenizer port) ,(number-tokenizer port))]
      [any-char (next-token port)]))
   (lxr port))
@@ -33,7 +33,7 @@
   (define lxr
     (lexer
      ["bot" (number-tokenizer port)]
-     ["output" (* -1 (number-tokenizer port))]
+     ["output" (+ 1000 (number-tokenizer port))]
      [any-char (bot-tokenizer port)]))
   (lxr port))
 
@@ -45,12 +45,14 @@
   (lxr port))
 
 ;; EXPANDER
+(require data/queue)
+
 (define-macro (bb-module-begin PARSE-TREE)
   #'(#%module-begin
      PARSE-TREE))
 (provide (rename-out [bb-module-begin #%module-begin]))
 
-(provide balance-bots-instructions compare give bot-instruction)
+(provide balance-bots-instructions compare give bot-rule)
 (provide #%app #%datum #%top-interaction)
 
 (define-macro (balance-bots-instructions INSTRUCTIONS ...)
@@ -65,60 +67,71 @@
 (define-macro (give V B)
   #'((curry do-give) V B))
 
-(define-macro (bot-instruction B L H)
-  #'((curry do-bot-instruction) B L H))
+(define-macro (bot-rule B L H)
+  #'((curry do-bot-rule) B L H))
 
 (define (fold-funcs instructions)
   (let ([final-env (for/fold ([env (environment (make-hash)
                                                 (make-hash)
                                                 (list 0 0)
-                                                +inf.0)])
+                                                (make-queue))])
                              ([instruction (in-list instructions)])
                      (apply instruction (list env)))])
-    (fprintf (current-output-port)
-             "The bot that compared ~a was ~a.\n"
-             (environment-comparison final-env)
-             (environment-comparison-bot final-env))))
+    (fprintf (current-output-port) "~a\n" (bfs final-env))))
 
 ;; LOGIC
-(struct environment (bot-slots bot-instructions comparison comparison-bot) #:transparent)
+
+
+(struct environment (bot-slots bot-rules comparison queue) #:transparent)
 
 (define (add-to-slots slots value)
   (cdr (sort (cons value slots) <)))
 
 (define (do-compare a b env)
   (environment (environment-bot-slots env)
-               (environment-bot-instructions env)
+               (environment-bot-rules env)
                (sort (list a b) <)
-               (environment-comparison-bot env)))
+               (environment-queue env)))
 
 (define (do-give v b env)
   (let* ([bot-slots (environment-bot-slots env)]
          [slots (hash-ref bot-slots b (list 0 0))]
-         [new-slots (add-to-slots slots v)]
-         [comparison (environment-comparison env)])
+         [new-slots (add-to-slots slots v)])
     (begin
       (hash-set! bot-slots b new-slots)
-      (let ([new-env (environment bot-slots
-                                  (environment-bot-instructions env)
-                                  (environment-comparison env)
-                                  (if (equal? comparison new-slots)
-                                      b
-                                      (environment-comparison-bot env)))])
-        (fprintf (current-output-port) "@ ~a compares ~a ~a\n" b new-slots comparison)
-        (if (and (> b 0) (foldl (λ (x y) (and x y)) #t (map (λ (x) (> x 0)) new-slots)))
-            (let* ([bot-instructions (hash-ref (environment-bot-instructions env) b)]
-                   [lo-bot (first bot-instructions)]
-                   [hi-bot (second bot-instructions)])
-              (fprintf (current-output-port) "% ~a gives ~a to ~a and ~a to ~a\n" b
-                       (first new-slots) lo-bot (second new-slots) hi-bot)
-              (do-give (second new-slots) hi-bot (do-give (first new-slots) lo-bot new-env)))
-            new-env)))))
+      (when (and (< b 1000) (> (first new-slots) 0) (> (second new-slots) 0))
+        (enqueue! (environment-queue env) b))
+      (environment bot-slots
+                   (environment-bot-rules env)
+                   (environment-comparison env)
+                   (environment-queue env)))))
 
-(define (do-bot-instruction b l h env)
-  (let ([all-bot-instructions (environment-bot-instructions env)])
-    (hash-set! all-bot-instructions  b (list l h))
+(define (do-bot-rule b l h env)
+  (let ([all-bot-rules (environment-bot-rules env)])
+    (hash-set! all-bot-rules  b (list l h))
     (environment (environment-bot-slots env)
-                 all-bot-instructions
+                 all-bot-rules
                  (environment-comparison env)
-                 (environment-comparison-bot env))))
+                 (environment-queue env))))
+
+(define (bfs env)
+  (if (empty? (environment-queue env))
+      +inf.0
+      (let* ([hd (dequeue! (environment-queue env))]
+             [all-slots (environment-bot-slots env)]
+             [hd-slots (hash-ref all-slots hd)])
+        (if (equal? hd-slots (environment-comparison env))
+            hd
+            (let* ([all-rules (environment-bot-rules env)]
+                   [hd-rules (hash-ref all-rules hd)]
+                   [lo-node (first hd-rules)]
+                   [hi-node (second hd-rules)]
+                   [lo-val (first hd-slots)]
+                   [hi-val (second hd-slots)])
+              (bfs (do-give hi-val hi-node (do-give lo-val
+                                                    lo-node
+                                                    (environment (environment-bot-slots env)
+                                                                 (environment-bot-rules env)
+                                                                 (environment-comparison env)
+                                                                 (environment-queue env))))))))))
+  
