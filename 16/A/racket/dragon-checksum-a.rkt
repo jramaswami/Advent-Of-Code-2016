33947e55bc370dfc0162e6035518962089c61081
jramaswami <jramaswami@gmail.com>
2016-12-16 09:53:51 -0500

    Changing direction ...

--- 16/A/racket/dragon-checksum-a.rkt
+++ 16/A/racket/dragon-checksum-a.rkt
@@ -1,17 +1,28 @@
 #lang racket
 
-(define (bitlist->bitstring xs)
-  (string-append* (map number->string xs)))
+(define (flip-bits n)
+    (let ([mask (- (arithmetic-shift 1 (integer-length n)) 1)])
+      (bitwise-xor n mask)))
 
-(define (bitstring->bitlist s)
-  (map (λ (x) (- (char->integer x) 48)) (string->list s)))
+(define (reverse-bits n)
+  (define (helper0 m x)
+    (if (= m 0)
+        x
+        (let ([x0 (arithmetic-shift x 1)]
+              [m0 (bitwise-and m 1)])
+          (helper0 (arithmetic-shift m -1) (bitwise-ior x0 m0)))))
+  (helper0 n 0))
 
-(define (reverse-not xs)
-  (define (helper0 xs0 acc)
-    (cond [(empty? xs0) acc]
-          [(= (car xs0) 1) (helper0 (cdr xs0) (cons 0 acc))]
-          [else (helper0 (cdr xs0) (cons 1 acc))]))
-  (helper0 xs '()))
+(define (reverse-not n)
+  (define (helper0 m x)
+    (if (= m 0)
+        x
+        (let ([x0 (arithmetic-shift x 1)]
+              [m0 (bitwise-and m 1)])
+          (if (= m0 0)
+              (helper0 (arithmetic-shift m -1) (bitwise-ior x0 1))
+              (helper0 (arithmetic-shift m -1) (bitwise-ior x0 0))))))
+  (helper0 n 0))
 
 (define (dragon-curve xs)
   (append xs '(0) (reverse-not xs)))
@@ -35,28 +46,28 @@
       (take data len)
       (fill-disk (dragon-curve data) len)))
 
-(module+ test1
-  (require rackunit)
-
-  (check-equal? (dragon-curve '(1)) '(1 0 0))
-  (check-equal? (dragon-curve '(0)) '(0 0 1))
-  (check-equal? (dragon-curve (bitstring->bitlist "11111"))
-                (bitstring->bitlist "11111000000"))
-  (check-equal? (dragon-curve (bitstring->bitlist "111100001010"))
-                (bitstring->bitlist "1111000010100101011110000"))
-
-  (check-equal? (checksum (bitstring->bitlist "110010110100"))
-                (bitstring->bitlist "100"))
-
-  (check-equal? (fill-disk (bitstring->bitlist "10000") 20)
-                (bitstring->bitlist "10000011110010000111"))
-
-  (check-equal? (checksum (fill-disk (bitstring->bitlist "10000") 20))
-                (bitstring->bitlist "01100")))
-
-(module+ main1
-  (define init (string-trim (read-line)))
-  (define len (read))
-  (displayln
-   (bitlist->bitstring
-    (checksum (fill-disk (bitstring->bitlist init) len)))))
+;(module+ test1
+;  (require rackunit)
+;
+;  (check-equal? (dragon-curve '(1)) '(1 0 0))
+;  (check-equal? (dragon-curve '(0)) '(0 0 1))
+;  (check-equal? (dragon-curve (bitstring->bitlist "11111"))
+;                (bitstring->bitlist "11111000000"))
+;  (check-equal? (dragon-curve (bitstring->bitlist "111100001010"))
+;                (bitstring->bitlist "1111000010100101011110000"))
+;
+;  (check-equal? (checksum (bitstring->bitlist "110010110100"))
+;                (bitstring->bitlist "100"))
+;
+;  (check-equal? (fill-disk (bitstring->bitlist "10000") 20)
+;                (bitstring->bitlist "10000011110010000111"))
+;
+;  (check-equal? (checksum (fill-disk (bitstring->bitlist "10000") 20))
+;                (bitstring->bitlist "01100")))
+;
+;(module+ main1
+;  (define init (string-trim (read-line)))
+;  (define len (read))
+;  (displayln
+;   (bitlist->bitstring
+;    (checksum (fill-disk (bitstring->bitlist init) len)))))
