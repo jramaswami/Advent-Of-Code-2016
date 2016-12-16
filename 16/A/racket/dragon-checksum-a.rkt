0fe834198bed0291b25f8d5ae28ddb3bb041f6d1
jramaswami <jramaswami@gmail.com>
2016-12-16 09:25:13 -0500

    Solution to Day 16 Part A in Racket.

---
+++ 16/A/racket/dragon-checksum-a.rkt
@@ -0,0 +1,62 @@
+#lang racket
+
+(define (bitlist->bitstring xs)
+  (string-append* (map number->string xs)))
+
+(define (bitstring->bitlist s)
+  (map (λ (x) (- (char->integer x) 48)) (string->list s)))
+
+(define (reverse-not xs)
+  (define (helper0 xs0 acc)
+    (cond [(empty? xs0) acc]
+          [(= (car xs0) 1) (helper0 (cdr xs0) (cons 0 acc))]
+          [else (helper0 (cdr xs0) (cons 1 acc))]))
+  (helper0 xs '()))
+
+(define (dragon-curve xs)
+  (append xs '(0) (reverse-not xs)))
+
+(define (checksum xs)
+  (define (helper0 xs0)
+    (if (empty? xs0)
+        '()
+    (let ([a (first xs0)]
+          [b (second xs0)])
+      (if (equal? a b)
+          (cons 1 (helper0 (drop xs0 2)))
+          (cons 0 (helper0 (drop xs0 2)))))))
+  (let ([cs (helper0 xs)])
+    (if (odd? (length cs))
+        cs
+        (checksum cs))))
+
+(define (fill-disk data len)
+  (if (<= len (length data))
+      (take data len)
+      (fill-disk (dragon-curve data) len)))
+
+(module+ test1
+  (require rackunit)
+
+  (check-equal? (dragon-curve '(1)) '(1 0 0))
+  (check-equal? (dragon-curve '(0)) '(0 0 1))
+  (check-equal? (dragon-curve (bitstring->bitlist "11111"))
+                (bitstring->bitlist "11111000000"))
+  (check-equal? (dragon-curve (bitstring->bitlist "111100001010"))
+                (bitstring->bitlist "1111000010100101011110000"))
+
+  (check-equal? (checksum (bitstring->bitlist "110010110100"))
+                (bitstring->bitlist "100"))
+
+  (check-equal? (fill-disk (bitstring->bitlist "10000") 20)
+                (bitstring->bitlist "10000011110010000111"))
+
+  (check-equal? (checksum (fill-disk (bitstring->bitlist "10000") 20))
+                (bitstring->bitlist "01100")))
+
+(module+ main1
+  (define init (string-trim (read-line)))
+  (define len (read))
+  (displayln
+   (bitlist->bitstring
+    (checksum (fill-disk (bitstring->bitlist init) len)))))
