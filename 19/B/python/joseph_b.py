3a726d2251592ed61e61d6e58755d65d5c3bf89a
jramaswami <jramaswami@gmail.com>
2016-12-19 10:33:42 -0500

    Initial commit for Day 19 Part B in Python.

--- 19/B/python/joseph_b.py
+++ 19/B/python/joseph_b.py
@@ -3,14 +3,14 @@ Advent of Code 2016
 Day 19, Part B
 An Elephant Named Joseph
 """
-
-from tqdm import tqdm
+# pylint: disable=invalid-name
 
 
 def white_elephant_party(elf_count):
     """
     Returns the "winner" of the elves
-    white elephant party.
+    white elephant party.  Uses simulation
+    to determine "winner."
     """
     circle = []
     for index in range(1, elf_count + 1):
@@ -18,29 +18,47 @@ def white_elephant_party(elf_count):
 
     index = 0
     len_circle = len(circle)
-    with tqdm(total=len_circle) as pbar:
-        while len_circle > 1:
-            vindex = (index + (len_circle // 2)) % len_circle
-            del circle[vindex]
-            len_circle = len_circle - 1
-            if index < vindex:
-                index = index + 1
-            index = index % len_circle
-            pbar.update(1)
+    while len_circle > 1:
+        vindex = (index + (len_circle // 2)) % len_circle
+        del circle[vindex]
+        len_circle = len_circle - 1
+        if index < vindex:
+            index = index + 1
+        index = index % len_circle
 
     return circle[0]
+
+
+def josephus_circle(n):
+    """
+    Returns the "winner" of the josephus circle. Uses
+    the following math to determine "winner":
+
+    where j(n) = j(3^x + m) = m if m ≤ 3^x
+                            = 3^x + 2(m - 3^x) if m > 3^x
+    """
+    # find power of three
+    x = 0
+    while pow(3, x) < n:
+        x = x + 1
+    x = x - 1
+    y = pow(3, x)
+    m = (n - y)
+    if m > y:
+        return y + (2 * (m - y))
+    else:
+        return m
 
 
 def main():
     """
     Main program
     """
-    # winning_elf = white_elephant_party(5)
-    # winning_elf = white_elephant_party(3001330)
     import sys
     if len(sys.argv) > 1:
         elf_count = int(sys.argv[1])
-        winning_elf = white_elephant_party(elf_count)
+        # winning_elf = white_elephant_party(elf_count)
+        winning_elf = josephus_circle(elf_count)
         print(winning_elf)
     else:
         print("Usage: python joseph_b <elf_count>")
--- 19/B/python/test_joseph_b.py
+++ 19/B/python/test_joseph_b.py
@@ -3,7 +3,7 @@ Tests for joseph_b.py
 """
 
 import unittest
-from joseph_b import white_elephant_party
+from joseph_b import josephus_circle, white_elephant_party
 
 
 class TestJosephB(unittest.TestCase):
@@ -17,6 +17,18 @@ class TestJosephB(unittest.TestCase):
         """
         winner = white_elephant_party(5)
         self.assertEqual(winner, 2)
+
+    def test_josephus_circle(self):
+        """
+        Tests for josephus_circle
+        """
+        self.assertEqual(josephus_circle(5), 2)
+        self.assertEqual(josephus_circle(16), 1)
+        self.assertEqual(josephus_circle(20), 13)
+        self.assertEqual(josephus_circle(27), 27)
+        self.assertEqual(josephus_circle(68), 55)
+        self.assertEqual(josephus_circle(94), 13)
+        self.assertEqual(josephus_circle(100), 19)
 
 
 if __name__ == '__main__':
