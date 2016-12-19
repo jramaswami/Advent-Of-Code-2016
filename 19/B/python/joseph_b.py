795feaac292afb4f7f52b613bf2d3a68b2c1de7e
jramaswami <jramaswami@gmail.com>
2016-12-19 10:37:29 -0500

    Solution to Day 19 Part B in Python.

--- 19/B/python/joseph_b.py
+++ 19/B/python/joseph_b.py
@@ -6,29 +6,6 @@ An Elephant Named Joseph
 # pylint: disable=invalid-name
 
 
-def white_elephant_party(elf_count):
-    """
-    Returns the "winner" of the elves
-    white elephant party.  Uses simulation
-    to determine "winner."
-    """
-    circle = []
-    for index in range(1, elf_count + 1):
-        circle.append(index)
-
-    index = 0
-    len_circle = len(circle)
-    while len_circle > 1:
-        vindex = (index + (len_circle // 2)) % len_circle
-        del circle[vindex]
-        len_circle = len_circle - 1
-        if index < vindex:
-            index = index + 1
-        index = index % len_circle
-
-    return circle[0]
-
-
 def josephus_circle(n):
     """
     Returns the "winner" of the josephus circle. Uses
@@ -44,10 +21,10 @@ def josephus_circle(n):
     x = x - 1
     y = pow(3, x)
     m = (n - y)
-    if m > y:
-        return y + (2 * (m - y))
-    else:
+    if m <= y:
         return m
+    else:
+        return y + (2 * (m - y))
 
 
 def main():
@@ -56,10 +33,7 @@ def main():
     """
     import sys
     if len(sys.argv) > 1:
-        elf_count = int(sys.argv[1])
-        # winning_elf = white_elephant_party(elf_count)
-        winning_elf = josephus_circle(elf_count)
-        print(winning_elf)
+        print(josephus_circle(int(sys.argv[1])))
     else:
         print("Usage: python joseph_b <elf_count>")
 
--- 19/B/python/test_joseph_b.py
+++ 19/B/python/test_joseph_b.py
@@ -3,27 +3,18 @@ Tests for joseph_b.py
 """
 
 import unittest
-from joseph_b import josephus_circle, white_elephant_party
+from joseph_b import josephus_circle
 
 
 class TestJosephB(unittest.TestCase):
     """
     Tests for joseph_b.py
     """
-
-    def test_white_elephant_party(self):
-        """
-        Tests for white_elephant_party
-        """
-        winner = white_elephant_party(5)
-        self.assertEqual(winner, 2)
-
     def test_josephus_circle(self):
         """
         Tests for josephus_circle
         """
         self.assertEqual(josephus_circle(5), 2)
-        self.assertEqual(josephus_circle(16), 1)
         self.assertEqual(josephus_circle(20), 13)
         self.assertEqual(josephus_circle(27), 27)
         self.assertEqual(josephus_circle(68), 55)
