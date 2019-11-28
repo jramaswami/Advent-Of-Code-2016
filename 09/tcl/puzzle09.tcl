504f84b5fba10aef06fabd5c5665786e3460a816
Joseph Ramaswami <jramaswami@gmail.com>
2019-11-28 18:26:15 -0500

    Solution to day 9 in Tcl.

--- 09/tcl/puzzle09.tcl
+++ 09/tcl/puzzle09.tcl
@@ -1,34 +1,31 @@
 # Advent of Code 2016 :: Day 9 :: Explosives in Cyberspace
 
-proc decompress {input {recursive 0}} {
-    set decompressed_string ""
+proc solve {input {recursive 0}} {
+    set soln 0
     set start_search 0
     set open_paren_index [string first "(" $input $start_search]
     while {$open_paren_index >= 0} {
-        append decompressed_string [string range $input $start_search [expr {$open_paren_index - 1}]]
+        incr soln [expr $open_paren_index - $start_search]
         set close_paren_index [string first ")" $input [expr {$open_paren_index + 1}]]
         set marker [string range $input [expr {$open_paren_index + 1}] [expr {$close_paren_index - 1}]]
         set tokens [split $marker "x"]
         set len [lindex $tokens 0]
         set reps [lindex $tokens 1]
-        set s [string range $input [expr {$close_paren_index + 1}] [expr {$close_paren_index + $len}]]
         if {$recursive} {
-            set t [string repeat [decompress $s 1] $reps]
+            set s [string range $input [expr {$close_paren_index + 1}] [expr {$close_paren_index + $len}]]
+            incr soln [expr {$reps * [solve $s 1]}]
         } else {
-            set t [string repeat $s $reps]
+            incr soln [expr {$len * $reps}]
         }
-        append decompressed_string $t
         set start_search [expr {$close_paren_index + $len + 1}]
         set open_paren_index [string first "(" $input $start_search]
     }
-    append decompressed_string [string range $input $start_search end]
-    return $decompressed_string
+    incr soln [expr {[string length $input] - $start_search}]
+    return $soln
 }
 
 if {$::argv0 eq [info script]} {
     set input [string trimright [read stdin]]
-    set decompressed [decompress $input]
-    puts "The solution to part 1 is [string length $decompressed]."
-    set decompressed [decompress $input 1]
-    puts "The solution to part 2 is [string length $decompressed]."
+    puts "The solution to part 1 is [solve $input]."
+    puts "The solution to part 2 is [solve $input 1]."
 }
